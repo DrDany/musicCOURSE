c70b90d2c35a2fb4f5ac94f92c296cdc219420fb
DrDany <ddv1551114@gmail.com>
2020-12-05 21:07:33 +0200

    add class contact

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contact:
+
+    def __init__(self, firstname, middle_name, last_name,
+                         nickname, company, home, mobile_phone, work,
+                         fax, homepage, bday, bmotnh, byear):
+        self.firstname - firstname
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.company = company
+        self.home = home
+        self.mobile_phone = mobile_phone
+        self.work = work
+        self.fax = fax
+        self.homepage = homepage
+        self.bday = bday
+        self.bmotnh = bmotnh
+        self.byear = byear
